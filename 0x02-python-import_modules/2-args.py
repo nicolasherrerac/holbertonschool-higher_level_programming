3fb001c95985c3d972d082e7bd22ea231be92daf
Nicolas <nicolasherrerac20@gmail.com>
2020-12-10 17:46:13 +0000

    args

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+import sys
+if len(sys.argv) == 1:
+    print("0 arguments.")
+if len(sys.argv) == 2:
+    print("1 argument:")
+if len(sys.argv) > 2:
+    print("{} arguments:".format(len(sys.argv) - 1))
+
+for i in range(1, len(sys.argv)):
+    print("{:d}: {}".format(i, (sys.argv[i])))

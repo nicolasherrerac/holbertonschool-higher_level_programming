d3c437818c1a355cd8b41c91fc931f0b64a6c62b
Nicolas <nicolasherrerac20@gmail.com>
2020-12-10 17:58:52 +0000

    Pep8

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
     if len(sys.argv) > 2:
         print("{} arguments:".format(len(sys.argv) - 1))
 
-        for i in range(1, len(sys.argv)):
-            print("{:d}: {}".format(i, (sys.argv[i])))
+    for i in range(1, len(sys.argv)):
+        print("{:d}: {}".format(i, (sys.argv[i])))

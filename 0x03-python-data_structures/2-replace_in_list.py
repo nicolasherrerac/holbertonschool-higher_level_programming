54ba0b28dbb0524d36e40e73c3e6bc37be66f61a
Nicolas <nicolasherrerac20@gmail.com>
2020-12-15 17:16:22 +0000

    change if and for

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,8 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-	if (my_list[idx] < 0 or my_list[idx] > len(my_list)):
-		return(my_list)
-	else:
-		for i in range(len(my_list)):
-			my_list[idx] = element
-		return(my_list)
-		
+    if (idx < 0 or idx >= len(my_list)):
+        return(my_list)
+    else:
+        for i in range(len(my_list) - 1):
+            my_list[idx] = element
+        return(my_list)

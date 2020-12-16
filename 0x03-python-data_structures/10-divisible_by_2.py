b1e65c52c86b53404b4613504eeb2a2cd6ec8c83
Nicolas <nicolasherrerac20@gmail.com>
2020-12-16 00:49:13 +0000

    Change for

--- 0x03-python-data_structures/10-divisible_by_2.py
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -2,7 +2,7 @@
 def divisible_by_2(my_list=[]):
     new = []
     if my_list:
-        for i in range(len(my_list)):
+        for i in my_list:
             if (i % 2 == 0):
                 new.append(True)
             else:

7ef3b66391cd30cef1a70a954c87a48992519812
Nicolas <nicolasherrerac20@gmail.com>
2020-12-16 00:21:08 +0000

    Fix error

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -2,17 +2,17 @@
 def add_tuple(tuple_a=(), tuple_b=()):
     a = [0, 0]
     b = [0, 0]
+    var1 = len(tuple_a)
+    if var1 >= 2:
+        var1 = 2
+    var2 = len(tuple_b)
+    if var2 >= 2:
+        var2 = 2
 
-    if len(tuple_a) > 2:
-        tuple_a = 2
-
-    if len(tuple_b) > 2:
-        tuple_b = 2
-
-    for i in range(len(tuple_a)):
+    for i in range(var1):
         a[i] += tuple_a[i]
 
-    for c in range(len(tuple_b)):
+    for c in range(var2):
         b[c] += tuple_b[c]
 
     return(a[0] + b[0], a[1] + b[1])

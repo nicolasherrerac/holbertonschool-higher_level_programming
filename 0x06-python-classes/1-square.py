811ab5c937995b68477f48e9950cc5e297b90bc5
Nicolas <nicolasherrerac20@gmail.com>
2021-01-05 22:38:43 +0000

    Comments

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
+"""This class define a square"""
+
+
 class Square:
+    """Class of square"""
     __size = 0
 
     def __init__(self, size):
+        """Private instance attribute"""
         self.__size = size

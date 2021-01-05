fc55b2a91a34ee29b6c93e611820f7b2f65039f1
Nicolas <nicolasherrerac20@gmail.com>
2021-01-05 22:54:58 +0000

    Comments

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -1,19 +1,27 @@
 #!/usr/bin/python3
+"""This class update private attribute"""
+
+
 class Square:
+    """Class of square"""
     __size = 0
 
     def __init__(self, size=0):
+        """Size"""
         self.__size = size
 
     def area(self):
+        """Global Area"""
         return self.__size ** 2
 
     @property
     def size(self):
+        """Size"""
         return self.__size
 
     @size.setter
     def size(self, value):
+        """Errors"""
         if type(value) is not int:
             raise TypeError("size must be an integer")
         if value < 0:

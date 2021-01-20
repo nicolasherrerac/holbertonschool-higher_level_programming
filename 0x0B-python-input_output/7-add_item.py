9583fb9422f4c4528db0996847eeee1b868cd026
Nicolas <nicolasherrerac20@gmail.com>
2021-01-20 23:22:48 +0000

    Fix error

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -6,9 +6,9 @@ save = __import__('5-save_to_json_file').save_to_json_file
 load = __import__('6-load_from_json_file').load_from_json_file
 
 try:
-    lista = load('add_item.json')
+    lista = load("add_item.json")
 except:
     lista = []
 
 lista.extend(sys.argv[1:])
-save(lista, 'add_item.json')
+save(lista, "add_item.json")

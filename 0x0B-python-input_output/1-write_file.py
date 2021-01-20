6115d2530737590205530437fde7dbbf2bfebf6f
Nicolas <nicolasherrerac20@gmail.com>
2021-01-20 22:28:34 +0000

    Write to a file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Function"""
+
+
+def write_file(filename="", text=""):
+    """Write in a file"""
+    with open(filename, mode='w' ,encoding="utf-8") as myfile:
+        myfile.write(text)
+        return len(text)

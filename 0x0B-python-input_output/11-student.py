bf2c2e4df4c2e492a83e9eeb05f270ea42ee82cb
Nicolas <nicolasherrerac20@gmail.com>
2021-01-20 22:31:37 +0000

    Student to disk and reload

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+"""Function"""
+
+
+class Student():
+    """Create class"""
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """same as 10-class_to_json.py"""
+        if attrs == None:
+            return self.__dict__
+        lista = {}
+        for i, c in self.__dict__.items():
+            if i in attrs:
+                lista[i] = c
+        return lista
+
+    def reload_from_json(self, json):
+        """replaces all attributes of the Student"""
+        self.__dict__.update(json)

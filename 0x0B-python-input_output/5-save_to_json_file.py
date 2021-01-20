efac0f9b441497c4a23d053c76e157094209f9cf
Nicolas <nicolasherrerac20@gmail.com>
2021-01-20 23:18:18 +0000

    Fix error

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def save_to_json_file(my_obj, filename):
     """Write object with JSON"""
-    with open(filename, "w", encoding="utf-8") as myfile:
+    with open(filename, "w") as myfile:
         return myfile.write(json.dumps(my_obj))
--- 0x0B-python-input_output/6-load_from_json_file.py
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -5,5 +5,5 @@ import json
 
 def load_from_json_file(filename):
     """Create obj with JSON"""
-    with open(filename, 'r', encoding="utf-8") as myfile:
+    with open(filename, 'r') as myfile:
         return json.load(myfile)

b533bea59403f276875f5d2e59961fe7eda0a6d0
Nicolas <nicolasherrerac20@gmail.com>
2021-01-20 22:27:42 +0000

    Read file

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Create function"""
+
+
+def read_file(filename=""):
+    """Read file txt"""
+    with open(filename, encoding="utf-8") as myfile:
+        print(myfile.read(), end="") 

87bec3a5f7d6364323f73a2f707afd095721bafc
Nicolas <nicolasherrerac20@gmail.com>
2021-01-27 15:41:44 +0000

    fix error

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -7,24 +7,30 @@ class Square(Rectangle):
     """Class Square"""
 
     def __init__(self, size, x=0, y=0, id=None):
+        """initializes the square"""
         super().__init__(size, size, x, y, id)
+        self.size = size
 
     @property
     def size(self):
+        """size"""
         return self.width
 
     @size.setter
+    """setter"""
     def size(self, value):
         self.width = value
         self.height = value
 
     def __str__(self):
+        """string representation of the rectangle"""
         return "[Square] ({}) {}/{} - {}".format(self.id,
                                                  self.x,
                                                  self.y,
                                                  self.width)
 
     def update(self, *args, **kwargs):
+        """updates attributes"""
         if args is not None and args is not ():
             for i, arg in enumerate(args):
                 if i == 0:
@@ -41,5 +47,6 @@ class Square(Rectangle):
                     setattr(self, key, value)
 
     def to_dictionary(self):
+        """dictionary of a Square"""
         return {'id': self.id, 'size': self.size,
                 'x': self.x, 'y': self.y}

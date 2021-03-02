59d6ea14afc3070f1511e5e47814cd417b893567
Nicolas <nicolasherrerac20@gmail.com>
2021-03-02 21:34:03 +0000

    Add auto_increment

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,4 +1,4 @@
 -- Script creates the database hbtn_0d_usa and the table states.
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL PRIMARY KEY, name VARCHAR(256));
+CREATE TABLE IF NOT EXISTS states (id INT UNIQUE NOT NULL AUTO_INCREMENT PRIMARY KEY, name VARCHAR(256));

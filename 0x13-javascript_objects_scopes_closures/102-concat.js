f3c267a4d6a827026aff5c2b7869062585b404b9
Nicolas <nicolasherrerac20@gmail.com>
2021-03-24 01:24:20 +0000

    Add Sync

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 const fs = require('fs');
-const a = fs.readFile(process.argv[2], 'utf8');
-const b = fs.readFile(process.argv[3], 'utf8');
-fs.writeFile(process.argv[4], a + b);
+const a = fs.readFileSync(process.argv[2], 'utf8');
+const b = fs.readFileSync(process.argv[3], 'utf8');
+fs.writeFileSync(process.argv[4], a + b);

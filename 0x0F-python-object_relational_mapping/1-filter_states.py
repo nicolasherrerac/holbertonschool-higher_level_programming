22e2a7cbcd16896894f35790af6247d000799a81
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-03-27 10:08:24 -0500

    Add ASC in execute

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,8 @@
 #!/usr/bin/python3
-"""Lists all states from the database hbtn_0e_0_usa"""
+"""
+    lists all states with a name starting with N
+    (upper N) from the database hbtn_0e_0_usa
+"""
 import MySQLdb
 from sys import argv
 
@@ -12,7 +15,7 @@ if __name__ == '__main__':
     db = MySQLdb.connect(host="localhost", port=3306, user=myuser,
                          passwd=mypass, db=mydata)
     cursor = db.cursor()
-    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id;")
+    cursor.execute("SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC;")
     r = cursor.fetchall()
     for x in r:
         print(x)

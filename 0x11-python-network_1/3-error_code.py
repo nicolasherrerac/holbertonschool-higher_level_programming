4a444746a11ced2ec14f3fcfe94ab0b4dfcd5b6f
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-04-13 20:13:37 -0500

    Error code

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""ERROR code"""
+import urllib.request
+import urllib.error
+from sys import argv
+
+if __name__ == "__main__":
+    """Display error"""
+    data = urllib.request.Request(argv[1])
+    try:
+        with urllib.request.urlopen(data) as reponse:
+            print(response.read().decode('utf-8'))
+    except urllib.error.HTTPError as error:
+        print("Error code : {}".format(error.code))

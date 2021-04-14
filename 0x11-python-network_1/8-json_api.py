03952941ed00c9aa9e174455e359752e4c017e0e
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-04-13 20:14:29 -0500

    Search API

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+"""takes in a letter and sends a POST request"""
+import requests
+from sys import argv
+
+
+if __name__ == "__main__":
+    if len(argv) < 2:
+        q = ""
+    else:
+        q = argv[1]
+        r = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        response = r.json()
+        if response == {}:
+            print("No result")
+        else:
+            print("[{}] {}".format(response.get("id"), response.get("name")))
+    except ValueError:
+        print("Not a valid JSON")

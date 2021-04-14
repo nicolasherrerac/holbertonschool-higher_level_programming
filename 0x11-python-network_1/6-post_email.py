ae2b7f99dea2ea4bea01a28ec13eb30dd37fb7ec
nicolasherrerac <nicolasherrerac20@gmail.com>
2021-04-13 20:14:09 -0500

    POST an email #1

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""POST email"""
+import requests
+from sys import argv
+
+if __name__ == '__main__':
+    values = {'email': argv[2]}
+    r = requests.post(argv[1], values)
+    print(r.text)
